27f03b031936289788c5c19d7e9c3695204bf8fe
Federico Menozzi <federicogmenozzi@gmail.com>
2016-06-15 20:38:20 -0400

    Simple push behavior for Git

--- setup.sh
+++ setup.sh
@@ -26,6 +26,7 @@ git config --global user.name "Federico Menozzi"
 git config --global user.email "federicogmenozzi@gmail.com"
 git config --global core.editor vim
 git config --global core.excludesfile ~/.gitignore_global
+git config --global push.default simple
 git config --global alias.co "checkout"
 git config --global alias.cm "commit"
 git config --global alias.br "branch"
