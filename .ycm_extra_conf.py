ff3538f2cbe290462049e685c81f3279a8889285
Federico Menozzi <federicogmenozzi@gmail.com>
2016-08-25 16:52:26 -0400

    Add some more default includes

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -25,9 +25,10 @@ flags = [
     # For a C project, you would set this to 'c' instead of 'c++'.
     '-x', 'c++',
     '-I', '/usr/include/',
+    '-I', '/usr/local/include',
     '-I', 'include',
     '-I', '../include',
-    '-I.',
+    '-I', '.'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
